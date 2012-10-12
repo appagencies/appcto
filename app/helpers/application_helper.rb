954fcd64b2e5ed018fa37650bf8baa0b61c7af11
sebmor <sebastian.mortelmans@gmail.com>
2012-10-12 23:20:37 +0200

    add logout icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def record_mix_panel(mixpanel_to_record)
     "<script type='text/javascript'>mpmetrics.track(#{mixpanel_to_record});</script>" if mixpanel_to_record.present?
   end
+
+  def cp(path)
+    "current" if current_page?(path)
+  end
 end
