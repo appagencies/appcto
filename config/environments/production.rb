92d8537b6596af104d9246eea3df07e42d733a2f
sebmor <sebastian.mortelmans@gmail.com>
2012-12-13 17:05:14 +0100

    add forget pass config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Appcto::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'pacific-depths-5743.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.mandrillapp.com',
